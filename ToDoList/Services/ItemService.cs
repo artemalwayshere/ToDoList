938327ab6127b900828895bdc97a90dba3174ad8
artemalwayshere <artem.alwayshere@gmail.com>
2022-09-29 23:15:56 +0600

    added item service class

---
+++ ToDoList/Services/ItemService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Services
+{
+    public class ItemService
+    {
+        private readonly IRepository repo; 
+
+        public ItemService(IRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public IEnumerable<ToDoItem> SortByDate(IRepository repo)
+        {
+            var responseByDate = repo.GetItemList().OrderBy(i => i.DateTimeCreate);
+            return responseByDate;
+        }
+    }
+}
